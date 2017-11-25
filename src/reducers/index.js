21cb53e7f8323321ca29d12854218c5311a503d7
Michael Campos <camposmj89@gmail.com>
2017-11-25 09:02:32 -0800

    Add new SelectionReducer to the redux store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux';
 import libraries from './LibraryReducer';
+import selectedLibraryId from './LibraryReducer';
 
 export default combineReducers({
-	libraries
+	libraries,
+	selectedLibraryId
 });
